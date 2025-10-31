773b3385f3dbb0c8d99089943d4e3c8b670b163b
Gaurav Vaidya <gaurav@ggvaidya.com>
2025-10-31 00:17:29 -0400

    Updated ChEBI download URLs.

--- src/datahandlers/chebi.py
+++ src/datahandlers/chebi.py
@@ -2,8 +2,8 @@ from src.babel_utils import pull_via_ftp
 
 
 def pull_chebi():
-    pull_via_ftp("ftp.ebi.ac.uk", "/pub/databases/chebi/SDF/", "ChEBI_complete.sdf.gz", decompress_data=True, outfilename="CHEBI/ChEBI_complete.sdf")
-    pull_via_ftp("ftp.ebi.ac.uk", "/pub/databases/chebi/Flat_file_tab_delimited/", "database_accession.tsv", outfilename="CHEBI/database_accession.tsv")
+    pull_via_ftp("ftp.ebi.ac.uk", "/pub/databases/chebi/SDF", "chebi.sdf.gz", decompress_data=True, outfilename="CHEBI/ChEBI_complete.sdf")
+    pull_via_ftp("ftp.ebi.ac.uk", "/pub/databases/chebi/flat_files", "database_accession.tsv.gz", decompress_data=True, outfilename="CHEBI/database_accession.tsv")
 
 
 def x(inputfile, labelfile, synfile):
