82b4998b8c95501b9281459df2b5b1d83806c88f
Gaurav Vaidya <gaurav@ggvaidya.com>
2025-12-09 17:23:03 -0500

    Improved script.

--- slurm/run-babel-on-slurm.sh
+++ slurm/run-babel-on-slurm.sh
@@ -6,7 +6,7 @@ sbatch <<EOF
 #SBATCH --output=babel_outputs/logs/sbatch-${BABEL_VERSION:-babel-current}.out
 #SBATCH --error=babel_outputs/logs/sbatch-${BABEL_VERSION:-babel-current}.err
 #SBATCH --time=${BABEL_TIMEOUT:-24:00:00}
-#SBATCH --mem=64G
+#SBATCH --mem=16G
 #SBATCH --nodes=1
 #SBATCH --chdir=$PWD
 
@@ -16,12 +16,15 @@ sbatch <<EOF
 
 source ~/.bashrc
 
-# Build anatomy related compendia in a distributed fashion as defined in slurm/config.yaml profile
+# Run Babel in a distributed fashion as defined in slurm/config.yaml profile
+#
 # Note that since Snakemake supports slurm executor plugin natively, submitting this as a SLURM batch
 # job is not recommended since that will create an outer SLURM job running Snakemake which then
 # submits innter SLURM jobs for workflow rules as specified in the profile. The recommended way
 # is to run this directly on the login or head node. However, it might not be a good thing to have
 # a long-running process on login/head nodes. So a good compromise is to still use the sbatch wrapper
 # to submit the snakemake job but request minimal resources for the outer job as shown in this job script.
-uv run snakemake --profile slurm
+
+uv run snakemake --profile slurm "$@"
+
 EOF
