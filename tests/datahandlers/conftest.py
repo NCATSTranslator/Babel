6c509c97168549874a13f45a1ef3e5fe0d53da2d
Gaurav Vaidya <gaurav@ggvaidya.com>
2026-05-10 14:53:51 -0400

    Documented issue for eventual improvements.

--- tests/datahandlers/conftest.py
+++ tests/datahandlers/conftest.py
@@ -2,6 +2,10 @@
 
 These are plain functions (not fixtures) so they can be imported by both unit tests
 and pipeline tests without going through pytest's fixture injection machinery.
+
+As per https://github.com/NCATSTranslator/Babel/issues/736, they should eventually
+be turned into a core part of Babel so that the same code can be used to read and
+write these internal files.
 """
 
 
