0f3a4b613b4d6fafaf021ec481cc76419112b2cf
Gaurav Vaidya <gaurav@renci.org>
2026-05-28 22:18:24 -0400

    Apply suggestion from @gaurav

--- src/prefixes.py
+++ src/prefixes.py
@@ -1,6 +1,7 @@
 # Prefixes used for CURIEs
 #
 # The point here is to keep prefixes in one place so that we can easily deal with things like PUBCHEM->PUBCHEM.COMPOUND
+# In principle, we should generate this off of the context file?
 
 MESH = "MESH"
 NCIT = "NCIT"
