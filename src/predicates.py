b67e069acd05ad3f445c44aa7e7c2eba67275f45
Gaurav Vaidya <gaurav@ggvaidya.com>
2026-05-28 21:45:31 -0400

    Rearranged predicates file for improved readability.

--- src/predicates.py
+++ src/predicates.py
@@ -4,13 +4,8 @@
 # SPARQL PREFIX declarations inside query strings are left as inline literals
 # since the SPARQL syntax requires it and they don't benefit from centralisation.
 
-OBO_IN_OWL = "http://www.geneontology.org/formats/oboInOwl#"
-SKOS = "http://www.w3.org/2004/02/skos/core#"
-RDFS = "http://www.w3.org/2000/01/rdf-schema#"
-OWL = "http://www.w3.org/2002/07/owl#"
-RDF = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-
 # oboInOwl predicates
+OBO_IN_OWL = "http://www.geneontology.org/formats/oboInOwl#"
 HAS_EXACT_SYNONYM = OBO_IN_OWL + "hasExactSynonym"
 HAS_RELATED_SYNONYM = OBO_IN_OWL + "hasRelatedSynonym"
 HAS_ALTERNATIVE_ID = OBO_IN_OWL + "hasAlternativeId"
@@ -19,16 +14,22 @@ HAS_SYNONYM = OBO_IN_OWL + "hasSynonym"
 OBO_ID = OBO_IN_OWL + "id"
 
 # SKOS predicates
+SKOS = "http://www.w3.org/2004/02/skos/core#"
 EXACT_MATCH = SKOS + "exactMatch"
 CLOSE_MATCH = SKOS + "closeMatch"
 ALT_LABEL = SKOS + "altLabel"
 PREF_LABEL = SKOS + "prefLabel"
 
+# RDF predicates
+RDF = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+
 # RDFS predicates
+RDFS = "http://www.w3.org/2000/01/rdf-schema#"
 RDFS_LABEL = RDFS + "label"
 RDFS_SUBCLASSOF = RDFS + "subClassOf"
 
 # OWL predicates
+OWL = "http://www.w3.org/2002/07/owl#"
 OWL_EQUIVALENT_CLASS = OWL + "equivalentClass"
 OWL_CLASS = OWL + "Class"
 OWL_ONTOLOGY = OWL + "Ontology"
